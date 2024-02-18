86b982eb2a945e104543bf621b925c31f6c80394
N Dieff <nick.dieff@gmail.com>
2024-02-18 11:46:47 -0500

    update artifact jobs in Actions to latest version

--- src/vismod_processing/influxDB/initialization.py
+++ src/vismod_processing/influxDB/initialization.py
@@ -1,12 +1,15 @@
-# This script is the corrected script provided by InfluxDB for database 
+# This script is the corrected script provided by InfluxDB for database
 # initialization
 
 
 import influxdb_client
+
 # import os
 import time
+
 # from influxdb_client import InfluxDBClient
 from influxdb_client import Point
+
 # from influxdb_client import WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -26,13 +29,13 @@ url = "http://influx:8086"
 write_client = influxdb_client.InfluxDBClient(url, token, organization)
 bucket = "main"
 write_api = write_client.write_api(write_options=SYNCHRONOUS)
-   
+
 for value in range(5):
     point = (
         Point("measurement1")
         .tag("tagname1", "tagvalue1")
         .field("field1", value)
-      )
+    )
     write_api.write(bucket=bucket, org="zatan", record=point)
     time.sleep(1)
 
