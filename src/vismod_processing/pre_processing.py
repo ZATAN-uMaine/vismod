99afb540a5b77a8f15fb600a492b3e998ae8e09e
tKharvud <tyler.harwood@maine.edu>
2024-02-23 04:57:42 -0500

    made some important changes to syncConfig.py, integrated pre-processing script with the format outlined there with only minor changes.

--- src/vismod_processing/pre_processing.py
+++ src/vismod_processing/pre_processing.py
@@ -1,5 +1,6 @@
 from nptdms import TdmsFile
 import pandas as pd
+from vismod_processing import syncConfig
 
 """
 
@@ -9,12 +10,14 @@ import pandas as pd
 class Pre_Processor:
     def __init__(self, calibration_file_path):
         """
-        This is just so we can have one pre_processor for each
-        calibration table if we have multiple that
-        need to be applied to the same data channels.
+        Todo: calib_table should be initialized with Alex's function
         """
         self.calib_file_path = calibration_file_path
-        self.calib_table = pd.DataFrame()
+        # calib_file = pd.read_excel(calibration_file_path)
+        self.calib_table = syncConfig.process_excel_to_dict(
+            calibration_file_path
+        )
+        # calib_file.close()
 
     def get_calibs_from_local_csv(self, csv_path, index_column="WDAQ"):
         table = pd.read_csv(csv_path, index_col=index_column)
@@ -29,13 +32,15 @@ class Pre_Processor:
         # Union with existing calb table
         self.calib_table = pd.concat([self.calib_table, table])
 
-    def get_calibs_from_local_xlsx(
+    def add_calibs_from_local_xlsx(
         self, calib_table_path, calib_table_columns, index_column="WDAQ"
     ):
         """
         Takes a pair of excel-style string of column names (ex. 'L,H')
         One column is the calibration factors, the other is the index used
         Adds it to the self.calib_table dictionary.
+
+        DO NOT USE, does not work with current format
         """
         table = pd.read_excel(
             calib_table_path,
@@ -79,12 +84,10 @@ class Pre_Processor:
         => dictionary of pandas data frames
         """
         tdms_dict = {}
-        indices = []
         with TdmsFile.open(path) as tdms_file:
             for group in tdms_file.groups():
-                indices.append(group.name)
                 tdms_dict = tdms_dict | {group.name: group.as_dataframe()}
-        return tdms_dict
+        return pd.DataFrame.from_dict(tdms_dict)
 
     def get_local_data_as_dataframe(self, path):
         """
@@ -95,38 +98,68 @@ class Pre_Processor:
 
     def apply_calibration(
         self,
-        remote_tdms_dict,
-        fun=lambda item, table, parameter, sensor, channel: item
-        * table["Cal Factor"][sensor + f"/{channel}"],
+        tdms_dict,
+        fun=lambda item, config, sensor, channel: item
+        * (config[sensor][channel]),
     ):
         """
-        Just apply a given function 'fun' to calib values
-        Suggested: fun = lambda i, t, p, s, c: i * t[p][s + f"/{c}"]
+        Just apply a given function 'fun' to calib valuesparameter
+        This function has no side-efffects. It does rely on the calib table
+        """
+
+        for sensor in self.calib_table.keys():
+            for channel in self.calib_table[sensor].keys():
+                # This is a lazy, but necessary check
+                # Calibs could use restructuring.
+                if channel[-8:] == "Cable ID":
+                    continue
+
+                colname = f"/'{sensor}'/'{channel}'"
+                tdms_dict[colname] = tdms_dict[colname].map(
+                    lambda item: fun(item, self.calib_table, sensor, channel)
+                )
+
+        return tdms_dict
+
+    def apply_calibration_map(
+        self,
+        remote_tdms_dict,
+        colcodes="L,T",
+        fun=lambda item, config, sensor, channel: item
+        * config[sensor][channel],
+    ):
+        """
+        Just apply a given function 'fun' to calib valuesparameter
+        This is a pure function, it has no side-efffects. It does rely on the calib table
         """
         tdms_dict = remote_tdms_dict.copy()
 
-        for parameter in self.calib_table.columns:
-
-            # For some reason it sometimes counts the colnames as a param.
-            if len(parameter.split("/")) == 1:
-                continue
-
-            sensor = parameter.split("/")[0]
-            channel = parameter.split("/")[1]
-
-            # Temporary try-except, needed to handle the two import methods
-            try:
-                tdms_dict[sensor][channel] = tdms_dict[sensor][channel].map(
-                    lambda item: fun(
-                        item, self.calib_table, parameter, sensor, channel
-                    )
-                )
-            except KeyError:
-                tdms_dict[f"/{sensor}/{channel}"] = tdms_dict[
-                    f"/{sensor}/{channel}"
-                ].map(
-                    lambda item: fun(
-                        item, self.calib_table, parameter, sensor, channel
-                    )
-                )
+        # Try-except just needed to handle the two import methods -temporary
+        # try:
+        #     for sensor in self.calib_table.keys():
+        #         tdms_dict[sensor] = tdms_dict[sensor].map(
+        #             lambda channel: map(
+        #                 fun(
+        #                     item,
+        #                     self.calib_table,
+        #                     sensor,
+        #                     channel
+        #                 ),
+        #                 tdms_dict[sensor][channel]
+        #             ),
+        #             tdms_dict[sensor]
+        #         )
+        # except KeyError:
+        #     tdms_dict = tdms_dict.map(
+        #         lambda colname:
+        #             map(
+        #                 fun(
+        #                     item,
+        #                     self.calib_table,
+        #                     colname.split('/')[0],
+        #                     colname.split('/')[1]
+        #                 ),
+        #                 tdms_dict[colname]
+        #             ),
+        #     )
         return tdms_dict
--- src/vismod_processing/syncConfig.py
+++ src/vismod_processing/syncConfig.py
@@ -9,41 +9,42 @@ def process_excel_to_dict(excel_file):
     # Extract contact info from Column A, skip unpopulated
     contact_info = df.iloc[4:, 0].dropna().tolist()
 
-    #TODO move this to DB # Save contact info to a text file
-    with open('contactinfo.txt', 'w') as file:
+    # TODO move this to DB # Save contact info to a text file
+    with open("contactinfo.txt", "w") as file:
         for info in contact_info:
             file.write(f"{info}\n")
 
     # Prepare dictionaries
     nested_dictionaries = {}
     for _, row in df.iloc[1:].iterrows():
-        if pd.notna(row['Node']):  # Check if column 'Node' is populated
-            node = row['Node']
+        if pd.notna(row["Node"]):  # Check if column 'Node' is populated
+            node = str(row["Node"])
             if node not in nested_dictionaries:
-                nested_dictionaries[node] = []  # Initialize a new list
-            
+                nested_dictionaries[node] = {}  # Initialize a new list
+
             dict_entry = {
                 # Cable ID is the label, row['Cable ID'] is the value
-                'Cable ID': row['Cable ID'],
+                "ch" + str(row["WDAQ_L"]) + "/Cable ID": row["Cable ID"],
                 # WDAQ channel number as key, 'Cal Factor' as value
-                row['WDAQ_L']: row['Cal Factor_L'],
+                "ch" + str(row["WDAQ_L"]): row["Cal Factor_L"],
                 # Since the TDMS files also use 'TEMP'
                 # we will also use it here for consistency
-                'TEMP': row['Cal Factor_T']
+                "TEMP": row["Cal Factor_T"],
             }
-            nested_dictionaries[node].append(dict_entry)
+            nested_dictionaries[node] = nested_dictionaries[node] | dict_entry
 
     return nested_dictionaries
 
 
-# Adjust the path to the Excel file as necessary
-excel_file_path = 'config.xlsx'
+if __name__ == "__main__":
+    # Adjust the path to the Excel file as necessary
+    excel_file_path = "config.xlsx"
 
-nested_dictionaries = process_excel_to_dict(excel_file_path)
-print(nested_dictionaries)
-# Convert dictionaries to JSON
-json_data = json.dumps(nested_dictionaries)
+    nested_dictionaries = process_excel_to_dict(excel_file_path)
+    print(nested_dictionaries)
+    # Convert dictionaries to JSON
+    json_data = json.dumps(nested_dictionaries)
 
-#TODO move this to DB # Write JSON data to file
-with open('data.json', 'w') as file:
-    file.write(json_data)
+    # TODO move this to DB # Write JSON data to file
+    with open("data.json", "w") as file:
+        file.write(json_data)
