385dbf37ffd2f24ac1542d7bdba99dfe1119d123
Andres0322 <93454368+Andres0322@users.noreply.github.com>
2024-02-23 13:24:37 -0500

    pushing more changes to upload data to db

--- src/vismod_processing/syncConfig.py
+++ src/vismod_processing/syncConfig.py
@@ -3,7 +3,7 @@ import os
 import json
 from influxdb_client import Point, InfluxDBClient, WriteOptions  
 
-zatanToken =  os.environ.get("INFLUXDB_TOKEN")
+
 
 
 def process_excel_to_dict(excel_file):
@@ -75,35 +75,37 @@ import json
 from influxdb_client import InfluxDBClient, Point, WriteOptions
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+
 point = Point("Calib_Table")
+
 data = json_data
 
-
-for key, value in data["sensors"].items():
+point = Point("Calib_Table")
+with open("data.json", "r") as json_file:
+    data = json.load(json_file)
+    point.tag("node", data["43641","43644","43642","45616","43643","45617"])
+    point.tag("channel", data["1","2"])
+    for key, value in data["1"].items():
+        point.field(key, value)
+    for key, value in data["2"].items():
         point.field(key, value)
 
-with InfluxDBClient.from_config_file("config.toml") as client:
-    with client.write_api(write_options=SYNCHRONOUS) as writer:
-        try:
-            writer.write(bucket="dev", record=[point])
-        except InfluxDBError as e:
-            print(e)
+
+
+with InfluxDBClient.from_env_properties() as client:
+    # use the client to access the necessary APIs
+    # for example, write data using the write_api
+    with client.write_api() as writer:
+        writer.write(bucket="dev", record=point)
+
+
 
 # # Convert dictionaries to InfluxDB JSON format
 # influxdb_data = []
 # for node, channels in nested_dictionaries.items():
 #     for channel, data in channels.items():
 #         influxdb_data.append({
-#             "measurement": measurement,
-#             "tags": {
-#                 "node": node,
-#                 "channel": channel
-#             },
-#             "fields": {
-#                 "cable_id": data["Cable ID"],
-#                 "temp": data["TEMP"]
-#             }
-#         })
+
 
 # # Write data to InfluxDB
 # client.write_points(influxdb_data)
