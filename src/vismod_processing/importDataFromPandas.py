bfee8f6c9ee1c3bb4ccf5ae5cd2897132d46f600
Andres0322 <93454368+Andres0322@users.noreply.github.com>
2024-02-24 00:35:34 +0000

    Pushing New importDataFromPandas script

--- src/vismod_processing/importDataFromPandas.py
+++ src/vismod_processing/importDataFromPandas.py
@@ -1,25 +1,39 @@
 import pandas as pd
 import os
+import logging
+import random
+from datetime import datetime
 from nptdms import TdmsFile
+from dotenv import load_dotenv
+from influxdb_client import Point, InfluxDBClient, WriteOptions  
+from influxdb_client.client.write_api import SYNCHRONOUS, PointSettings
+from influxdb_client.extras import pd, np
 
 # Import ZATAN's Pre Processor Class
 import pre_processing
 
-# Import ZATAN's Config Sync Script
-import syncConfig
+# Load environment
+load_dotenv(dotenv_path=Path(".env"))
 
-from influxdb_client import Point, InfluxDBClient, WriteOptions  
 
-# from influxdb_client.client.write_api import SYNCHRONOUS
+# Enable logging for DataFrame serializer
 
-# InfluxDB Token (NEED TO LEARN HOW TO PUT THIS IN THE ENV AND REFERENCE IT)
-# Currently I type INFLUXDB_TOKEN=<token> into the console to load it into the
-# environment prior to running the script
+loggerSerializer = logging.getLogger('influxdb_client.client.write.dataframe_serializer')
+loggerSerializer.setLevel(level=logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s | %(message)s'))
+loggerSerializer.addHandler(handler)
 
+# Load database secrets
 zatanToken = os.environ.get("INFLUXDB_V2_TOKEN")
+organization = os.environ.get("INFLUXDB_V2_ORG")
+link = os.environ.get("INFLUXDB_V2_URL")
 
+bucket = "dev"
+
+# set paths for necessary files
 pathToFile = "../../tests/081523.tdms"
-calib_table_path = '/raw-DAQ-files/sensorCalib.xlsx'
+calibTablePath = '/raw-DAQ-files/sensorCalib.xlsx'
 
 print("initializing processor")
 # Creating a Pre_Processor instance
@@ -29,7 +43,8 @@ print("\n Printint calib table")
 print(processor.calib_table)
 print("\n")
 
-_data_frame = processor.get_local_data_as_dataframe(pathToFile)
+# Initialize data frame
+dataFrame = processor.get_local_data_as_dataframe(pathToFile)
 print("initialized Dataframe")
 
 
@@ -38,51 +53,51 @@ def multiplier(item, table, sensor, channel):
 
 
 print("MADE IT TO DATA PROCESSOR")
-tdms_dict = processor.apply_calibration(
-   _data_frame, fun=multiplier
+tdmsDict = processor.apply_calibration(
+   dataFrame, fun=multiplier
 )
 print("ESCAPED DATA PROCESSOR")
-print(tdms_dict)
+print(tdmsDict)
+# End data frame initialization
+
+# Initialize Database Client
+print()
+print("=== Ingesting DataFrame via batching API ===")
+print()
+startTime = datetime.now()
+with InfluxDBClient(url=link, token=zatanToken, org=organization) as client:
+
+    # Use batching API
+    with client.write_api() as write_api:
+        write_api.write(bucket=bucket, record=dataFrame,
+                        data_frame_tag_columns=['tag'],
+                        data_frame_measurement_name="measurement_name")
+        print()
+        print("Wait to finishing ingesting DataFrame...")
+        print()
+
+print()
+print(f'Import finished in: {datetime.now() - startTime}')
+print()
 
 
-# # InfluxDB URL - if on dev container use "influx:8086", if on host
-# # machine running docker use "localhost:8086". ("127.0.0.1:8086")
-# # with InfluxDBClient(url="http://influx:8086", token="zatanToken", org="zatan") as _client:
-# with InfluxDBClient(url="http://localhost:8086", token="zatanToken", org="zatan") as _client:
-#     with _client.write_api(write_options=WriteOptions(batch_size=500,
-#                                                       flush_interval=10_000,
-#                                                       jitter_interval=2_000,
-#                                                       retry_interval=5_000,
-#                                                       max_retries=5,
-#                                                       max_retry_delay=30_000,
-#                                                       max_close_wait=300_000,
-#                                                       exponential_base=2)
-#                             ) as _write_client:
-#         # Import Pandas DataFrame (needs to be updated further along development)
-    
+# Generate Dataframe
+# """
+# print()
+# print("=== Generating DataFrame ===")
+# print()
+# dataframe_rows_count = 150_000
 
+# col_data = {
+#     'time': np.arange(0, dataframe_rows_count, 1, dtype=int),
+#     'tag': np.random.choice(['tag_a', 'tag_b', 'test_c'], size=(dataframe_rows_count,)),
+# }
+# for n in range(2, 2999):
+#     col_data[f'col{n}'] = random.randint(1, 10)
 
-#         pre_processing.Pre_Processor.apply_calibration()
+# data_frame = pd.DataFrame(data=col_data).set_index('time')
+# print(data_frame)
 
-
-
-
-
-# def main():
-#    # nested_dictionaries = syncConfig.process_excel_to_dict(excel_file_path)
-#    # print(" \n Printing the Dictionary: \n ")
-#     #print(nested_dictionaries)
-#     #print(" end dictionary \n")
-
-#     _data_frame = pd.DataFrame()
-#     importFrameFromFile(_data_frame, pathToFile)
-#     print(" \n Printing the Dataframe: \n ")
-#     print(_data_frame)
-#     print(" end dataframe \n")
-#     _write_client.write("dev", "zatan", record=_data_frame, data_frame_measurement_name='PNB_Reading')
-#     #                     data_frame_tag_columns=['location'])
-#     print("main")
-#     return 0
-
-
-# main()
+"""
+Ingest DataFrame
+"""
