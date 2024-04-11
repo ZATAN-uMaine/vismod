8ccd4691e2eb271bed102e0ab5c3acab50b79980
Alex <64880908+Spexivus@users.noreply.github.com>
2024-04-11 19:26:22 -0400

    Fix filename export

--- src/vismod_web/static/index.js
+++ src/vismod_web/static/index.js
@@ -50,7 +50,10 @@ function dateToIso(date) {
  * @returns string YYYY-MM-DD
  */
 function prettyPrintDate(date) {
-    return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`;
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
 }
 
 
